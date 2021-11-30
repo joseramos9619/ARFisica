13c5c000b6899e041cac1b1810d94a82ea60c4ce
Jose Antonio Ramos <65469289+joseramos9619@users.noreply.github.com>
2021-11-30 17:35:14 -0500

    Jose Ramos
    Registro con apoyo en php

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject m_loginUi = null;
+    [SerializeField] private GameObject m_registerUi = null;
+
+    [SerializeField] private InputField m_userInput = null;
+    [SerializeField] private InputField m_emailInput = null;
+    [SerializeField] private InputField m_passInput = null;
+    [SerializeField] private InputField m_veripassInput = null;
+    [SerializeField] private Text m_mens = null;
+
+    private Netword m_netword = null;
+
+    private void Awake()
+    {
+        m_netword = GameObject.FindObjectOfType<Netword>();
+    }
+
+    public void verregisterui()
+    {
+        m_loginUi.SetActive(false);
+        m_registerUi.SetActive(true);
+    }
+
+    public void veruserui()
+    {
+        m_loginUi.SetActive(true);
+        m_registerUi.SetActive(false);
+    }
+
+    public void Registrar()
+    {
+        if (m_userInput.text == "" || m_emailInput.text == "" || m_passInput.text == "" || m_veripassInput.text == "")
+        {
+            m_mens.text = "Por favor llene todos los campos";
+            return;
+        }
+
+        if (m_passInput.text == m_veripassInput.text)
+        {
+            m_mens.text = "Procesando...";
+
+            m_netword.Guardar(m_userInput.text, m_emailInput.text, m_passInput.text, delegate (Response response)
+            {
+                m_mens.text = response.mens;
+            });
+        }
+        else
+        {
+            m_mens.text = "La contraseña no coinside";
+        }
+    }
+}
---
+++ Assets/Scripts/Netword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Netword : MonoBehaviour
+{
+    public void Guardar(string user, string email, string pass, Action<Response> response)
+    {
+        StartCoroutine(CO_Guardar(user, email, pass, response));
+    }
+
+    private IEnumerator CO_Guardar(string user, string email, string pass, Action<Response> response)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", user);
+        form.AddField("email", email);
+        form.AddField("pass", pass);
+        WWW w = new WWW("http://localhost/User/Guardar.php", form);
+
+        yield return w;
+ 
+         response(JsonUtility.FromJson<Response>(w.text));
+    }
+}
+[Serializable]
+public class Response
+{
+    public bool done = false;
+    public string mens = "";
+}
