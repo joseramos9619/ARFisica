002809fb8f5c5d6acd4db18ed1cd95a1a29ed3ff
Jose Antonio Ramos <65469289+joseramos9619@users.noreply.github.com>
2021-12-02 15:31:46 -0500

    Jose Ramos
    Funcionalidad de ingreso y registro con hosting

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,9 +3,15 @@ using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
+    [Header("login")]
+    [SerializeField] private InputField m_loginUserInput = null;
+    [SerializeField] private InputField m_loginPassInput = null;
+    [SerializeField] private Text m_mensUser = null;
+    [SerializeField] private GameObject m_conteUi = null;
+
+    [Header("Registro")]
     [SerializeField] private GameObject m_loginUi = null;
     [SerializeField] private GameObject m_registerUi = null;
-
     [SerializeField] private InputField m_userInput = null;
     [SerializeField] private InputField m_emailInput = null;
     [SerializeField] private InputField m_passInput = null;
@@ -18,6 +24,28 @@ public class Manager : MonoBehaviour
     {
         m_netword = GameObject.FindObjectOfType<Netword>();
     }
+    
+    public void limpiar(){
+        m_loginUserInput.text="";
+        m_loginPassInput.text="";
+        m_userInput.text="";
+        m_passInput.text="";
+        m_emailInput.text="";
+        m_veripassInput.text="";
+        m_userInput.text="";
+        m_mens.text="";
+    }
+
+    public void cancelar(){
+        limpiar();
+        veruserui();
+    }
+
+    public void salir(){
+        limpiar();
+        m_loginUi.SetActive(true);
+        m_conteUi.SetActive(false);
+    }
 
     public void verregisterui()
     {
@@ -29,6 +57,33 @@ public class Manager : MonoBehaviour
     {
         m_loginUi.SetActive(true);
         m_registerUi.SetActive(false);
+    }
+
+    public void vercont(){
+        m_loginUi.SetActive(false);
+        m_registerUi.SetActive(false);
+        m_conteUi.SetActive(true);
+    }
+
+    public void Ingresar()
+    {
+        if (m_loginUserInput.text == "" || m_loginPassInput.text == "")
+        {
+            m_mens.text = "Por favor llene todos los campos";
+            return;
+        }
+        m_mens.text = "Procesando...";
+
+        m_netword.Validar(m_loginUserInput.text, m_loginPassInput.text, delegate (Response response)
+        {
+            m_mens.text = "Biembenido: "+response.mens;
+
+            if(m_loginUserInput.text==response.mens){
+                m_mensUser.text= "Usuario: "+response.mens;
+                vercont();
+                limpiar();
+            }
+        });
     }
 
     public void Registrar()
@@ -46,6 +101,11 @@ public class Manager : MonoBehaviour
             m_netword.Guardar(m_userInput.text, m_emailInput.text, m_passInput.text, delegate (Response response)
             {
                 m_mens.text = response.mens;
+            if (m_mens.text == "Registro Exitoso")
+            {
+                veruserui();
+                limpiar();
+            }
             });
         }
         else
--- Assets/Scripts/Netword.cs
+++ Assets/Scripts/Netword.cs
@@ -16,10 +16,27 @@ public class Netword : MonoBehaviour
         form.AddField("user", user);
         form.AddField("email", email);
         form.AddField("pass", pass);
-        WWW w = new WWW("http://localhost/User/Guardar.php", form);
+        WWW w = new WWW("https://developmentontime.000webhostapp.com/User/Guardar.php", form);
 
         yield return w;
  
+         response(JsonUtility.FromJson<Response>(w.text));
+    }
+
+    public void Validar(string user, string pass, Action<Response> response)
+    {
+        StartCoroutine(CO_Validar(user, pass, response));
+    }
+
+    private IEnumerator CO_Validar(string user, string pass, Action<Response> response)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", user);
+        form.AddField("pass", pass);
+        WWW w = new WWW("https://developmentontime.000webhostapp.com/User/Validar.php", form);
+
+        yield return w;
+        
          response(JsonUtility.FromJson<Response>(w.text));
     }
 }
