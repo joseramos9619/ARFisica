1fd6693bc55449ba708a81e6fbec94e6ccc5de9b
Jose Antonio Ramos <65469289+joseramos9619@users.noreply.github.com>
2021-12-11 14:59:30 -0500

    Jose Ramos
    Ajuste en la interfas, Navegacion entre opciones fluida

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Question : MonoBehaviour
-{   [Header("Cuestio")]
+{
+    [Header("Cuestio")]
     [SerializeField] private ToggleGroup m_p1 = null;
     [SerializeField] private ToggleGroup m_p2 = null;
     [SerializeField] private ToggleGroup m_p3 = null;
     [SerializeField] private ToggleGroup m_p4 = null;
     [SerializeField] private ToggleGroup m_p5 = null;
-    
+
     [Header("Alerta")]
-     [SerializeField] private GameObject m_alertUi = null;
+    [SerializeField] private GameObject m_alertUi = null;
     [SerializeField] private Text m_text = null;
 
-    string[] res=new string[5];
-    string[] sol=new string[5] {"A","B","C","A","C"};
+    private Inicio m_inicio = null;
+
+    private void Awake()
+    {
+        m_inicio = GameObject.FindObjectOfType<Inicio>();
+    }
+
+    string[] res = new string[5];
+    string[] sol = new string[5] { "A", "B", "C", "A", "C" };
 
     Toggle GetSelectedToggle(ToggleGroup res)
     {
         Toggle[] toggles = res.GetComponentsInChildren<Toggle>();
         foreach (var t in toggles)
-            if (t.isOn) return t; 
-        return null;           
+            if (t.isOn) return t;
+        return null;
+    }
+
+    public void desmarcar(ToggleGroup s)
+    {
+        Toggle[] toggles = s.GetComponentsInChildren<Toggle>();
+        foreach (var t in toggles)
+        {
+            if (t.isOn){
+                t.isOn=false;
+            }
+        }
     }
 
     public void select()
@@ -36,20 +55,22 @@ public class Question : MonoBehaviour
 
         if (p1 != null & p2 != null & p3 != null & p4 != null & p5 != null)
         {
-            int r=0;
-            res[0]=p1.name;
-            res[1]=p2.name;
-            res[2]=p3.name;
-            res[3]=p4.name;
-            res[4]=p5.name;
+            int r = 0;
+            res[0] = p1.name;
+            res[1] = p2.name;
+            res[2] = p3.name;
+            res[3] = p4.name;
+            res[4] = p5.name;
 
-            for(int i=0;i<5;i++){
-                if(res[i]==sol[i]){
+            for (int i = 0; i < 5; i++)
+            {
+                if (res[i] == sol[i])
+                {
                     r++;
                 }
             }
             m_alertUi.SetActive(true);
-            m_text.text="Nota: "+r;
+            m_text.text = "Nota: " + r;
         }
         else
         {
@@ -57,8 +78,20 @@ public class Question : MonoBehaviour
             m_text.text = "Completar todas las preguntas";
         }
     }
-    public void Cerrar(){
+    public void Cerrar()
+    {
         m_alertUi.SetActive(false);
         m_text.text = "";
+    }
+
+    public void cancelar()
+    {
+        desmarcar(m_p1);
+        desmarcar(m_p2);
+        desmarcar(m_p3);
+        desmarcar(m_p4);
+        desmarcar(m_p5);
+
+        m_inicio.volver();
     }
 }
